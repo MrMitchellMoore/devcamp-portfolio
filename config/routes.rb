799729d89255174df58eaa97aaafc66d3924f8f0
Mitchell Moore <mmoorejr24@gmail.com>
2020-02-05 22:03:21 -0800

    Itegrated custom actions for show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do  
   
   #Resources
-  resources :blogs
   resources :portfolios, except: [:show]
 
   # Independent routes
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
+  # Custom routes
+  resources :blogs do
+  	member do
+  		get :toggle_status	
+  	end
+  end
 
   root to: 'pages#home' 
 end
