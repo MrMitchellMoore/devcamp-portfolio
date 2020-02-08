30f4959482cf7af3240490a0abf955652cfceee2
Mitchell Moore <mmoorejr24@gmail.com>
2020-02-07 20:15:20 -0800

    implemented custom scopes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get 'angular-items', to: 'portfolios#angular'
 
   # Custom routes
   resources :blogs do
