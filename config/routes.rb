cea8e4efaf9200f8283b8e3d6e1bba260678a898
Mitchell Moore <mmoorejr24@gmail.com>
2020-02-11 12:48:59 -0800

    Added guest user for current_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do  
   
   # Auth routes  
-  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'} 
   
   #Resources
   resources :portfolios, except: [:show]
