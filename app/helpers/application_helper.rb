bae147022ad219f1e537da207f2a7d276de0ee0e
Mitchell Moore <mmoorejr24@gmail.com>
2020-02-19 10:58:42 -0800

    Updated login page and nav links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-	def login_helper(style)
+	def login_helper
 		if current_user.is_a?(GuestUser)
-      		(link_to "Log In", new_user_session_path, class: style) +
+      		(link_to "Log In", new_user_session_path, class: active_class(new_user_session_path)) +
       		" ".html_safe +
-      		(link_to "Register", new_user_registration_path, class: style) 
+      		(link_to "Register", new_user_registration_path, class: active_class(new_user_registration_path)) 
     	else 
-      		link_to "Logout", destroy_user_session_path, method: :delete, class: style 
+      		link_to "Logout", destroy_user_session_path, method: :delete, class: active_class(destroy_user_session_path) 
     	end		
 	end
 	
@@ -18,6 +18,10 @@ module ApplicationHelper
 
 	def copyright_generator
 		MooreViewTool::Renderer.copyright('Mitchell Moore', 'All rights reserved')
+	end
+
+	def active_class(link_path)
+		current_page?(link_path) ? 'nav-link active' : "nav-link"
 	end
 end
 
